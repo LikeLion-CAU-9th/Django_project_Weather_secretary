a6a5324fd3844a2b2a2a91071965735029ae6a68
SeongminLee <seongminlee221@gmail.com>
2021-05-12 00:40:06 +0900

    outfit.html modified

--- weather_secretary/weather_secretary/settings.py
+++ weather_secretary/weather_secretary/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'outfit',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'weather_secretary', 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
